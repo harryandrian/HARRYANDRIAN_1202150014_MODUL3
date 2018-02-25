9b29beff54981956f9fd138421a82ba989167529
muhtsanymalik <muhtsanymalik@gmail.com>
2018-02-26 00:01:12 +0700

    first commit

---
+++ app/src/main/java/com/example/harry/harryandrian_1202150014_modul3/AirMineral/ItemClickListener.java
@@ -0,0 +1,9 @@
+package com.example.harry.harryandrian_1202150014_modul3.AirMineral;
+
+/**
+ * Created by harryandrian on 2/25/2018.
+ */
+
+public interface ItemClickListener {
+    void onItemClick(int pos);
+}
---
+++ app/src/main/java/com/example/harry/harryandrian_1202150014_modul3/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.example.harry.harryandrian_1202150014_modul3;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent a = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(a);
+                finish();
+            }
+        },4000);
+    }
+}
+
